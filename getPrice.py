02a8971bc25fe03e9f8b2a5aa8879213b8d5fb93
johess123 <s109213067@mail1.ncnu.edu.tw>
2023-05-17 19:52:19 +0800

    change_url

--- getPrice.py
+++ getPrice.py
@@ -1,72 +1,34 @@
-from dbConfig import conn,cur
+import csv
 import requests
-from bs4 import BeautifulSoup
+from dbConfig import conn,cur
 import datetime
-# 爬取所有股票當日收盤價
 
 def main():
-    print("開始寫入各股收盤價...")
-    # 當天日期 (要再改)
-    nowTime = datetime.datetime.now()
-    nowTime1 = nowTime.strftime("%Y-%m-%d %H:%M:%S")
-    date = nowTime1[0:10]
-    # 爬股價
-    headers = {
-        'content-type': 'text/html; charset=UTF-8',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36\
-        (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
-    }
-    url = "https://histock.tw/stock/rank.aspx?p=all"
-    # 重複抓直到成功
-    do = True
-    while do:
+    # 今天日期
+    date = datetime.datetime.now().strftime("%Y%m%d")
+    #date = "20230517"
+    # 爬取股票資訊
+    print("爬取股票資訊...")
+    while True:
+        # 重複爬取直到成功
         try:
-            r = requests.get(url,headers=headers)
-            soup = BeautifulSoup(r.text, "html.parser")
-            allStock = soup.find('table',id='CPHB1_gv')
-            allStock = allStock.getText().split("\n")
-            do = False
+            url = "https://www.twse.com.tw/rwd/zh/afterTrading/MI_INDEX?date="+date+"&type=ALL&response=csv"
+            file = requests.get(url).text.split("\n")
+            break
         except:
             pass
-    print("爬蟲抓取完畢,開始存入DB")
-    # 取出所有需要存的股票代號
-    sql = "select * from allstock;" # 檢查這支股票有沒有在股票名稱table裡
-    cur.execute(sql,())
-    record = cur.fetchall()
-    allStockID = []
-    for i in record:
-        allStockID.append(i[0])
-    # 處理資料並寫入資料庫
-    i = 4
-    while i < len(allStock)-1:
-        thisStockid = allStock[i]
-        thisStockprice = float(allStock[i+3])*100 # 因精準度問題,所以存的價格*100,需取出計算時再除以100
-        # 是要儲存的股票
-        if thisStockid in allStockID:
-            allStockID.remove(thisStockid)
-            #sql = "INSERT INTO allprice(stockid,date,stockprice) values (%s,%s,%s);"
-            #cur.execute(sql,(thisStockid,date,thisStockprice))
-            #print(thisStockid,thisStockprice)
-        i += 16
-    conn.commit()
-    print("開始寫入缺漏的股票價格...")
-    # 把剩下沒爬到的股價分別爬取存進DB
-    for i in range(len(allStockID)):
-        url = "https://tw.stock.yahoo.com/quote/"+str(allStockID[i])+".TWO"
-        # 重複抓直到成功
-        do = True
-        while do:
-            try:
-                r = requests.get(url,headers=headers)
-                soup = BeautifulSoup(r.text, "html.parser")
-                stockprice = soup.find("span",{"class":"Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c) C($c-trend-up)"})
-                #sql = "INSERT INTO allprice(stockid,date,stockprice) values (%s,%s,%s);"
-                #cur.execute(sql,(allStockID[i],date,stockprice.getText()))
-                print(allStockID[i],stockprice.getText())
-                do = False
-                conn.commit()
-            except:
-                pass
-    print("各股收盤價寫入完成!")
-
+    date = date[0:4]+"-"+date[4:6]+"-"+date[6:]
+    # 今天是否有開盤
+    if file != ['']:
+        print("開始寫入各股收盤價...")
+        for i in range(271,len(file)-7):
+            stock = file[i].split('","')
+            if stock[8] != "--":
+                #print(stock[0].replace("=","").replace('"',""),stock[1],stock[8].replace(",",""))
+                sql = "INSERT INTO allprice(stockid,date,stockprice) values (%s,%s,%s);"
+                cur.execute(sql,(stock[0].replace("=","").replace('"',""),date,float(stock[8].replace(",",""))*100))
+        conn.commit()
+        print("寫入完成!")
+    else:
+        print("今日沒有開盤")
 main()
--- getstockid.py
+++ getstockid.py
@@ -1,23 +1,11 @@
+import csv
 from dbConfig import conn,cur
-import requests
-from bs4 import BeautifulSoup
-# 爬取所有股票代號和名稱
 
-def main():
-    headers = {
-        'content-type': 'text/html; charset=UTF-8',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36\
-        (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
-    }
-    url = "https://stock.wespai.com/p/3752"
-    r = requests.get(url,headers=headers)
-    soup = BeautifulSoup(r.text, "html.parser")
-    stockid = soup.find_all("td")
-    for i in range(0,len(stockid),3):
-        #print(i,stockid[i].getText())
-        #print(i,stockid[i+1].getText())
+# 寫入全部股票代號,名稱
+with open('MI_INDEX_ALL_20230517.csv', newline='') as csvfile:
+    rows = list(csv.reader(csvfile))
+    for i in range(271,len(rows)-6):
+        #print(rows[i][0].replace('"',"").replace("=",""),rows[i][1],rows[i][8])
         sql = "INSERT INTO allstock(stockid,stockname) values (%s,%s);"
-        cur.execute(sql,(stockid[i].getText(),stockid[i+1].getText()))
+        cur.execute(sql,(rows[i][0].replace('"',"").replace("=",""),rows[i][1]))
     conn.commit()
-
-main()
